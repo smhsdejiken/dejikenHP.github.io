fe044e33c4aff99b724256d337b8c8844d3df26d
seven3000 <pingjingyaoping@gmail.com>
2024-11-10 15:23:03 +0900

    Update BLOG-search.js

--- BLOG/BL-js/BLOG-search.js
+++ BLOG/BL-js/BLOG-search.js
@@ -5,7 +5,7 @@ searchInput.addEventListener('input', () => {
   const keyword = searchInput.value.toLowerCase();
   const articles = [
     { title: '#1 便利なWebアプリを、 ChatGPTと Googleサイト（無料）で作ろう!!', url: '../BL-html/BLOG1.html' },
-    { title: '#2 今日からあなたも！！', url: '../BL-html/BLOG2.html' },
+    { title: '#2 今日からあなたもデジ研部員！！', url: '../BL-html/BLOG2.html' },
   ];
 
   const results = articles.filter(article => article.title.toLowerCase().includes(keyword));
