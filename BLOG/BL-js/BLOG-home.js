edbe4390e5771e77bac5d62d58d90b0bdbceac5f
seven3000 <pingjingyaoping@gmail.com>
2024-12-30 00:49:38 +0900

    Update BLOG-home.js

--- BLOG/BL-js/BLOG-home.js
+++ BLOG/BL-js/BLOG-home.js
@@ -22,5 +22,23 @@ function gate(){
    document.getElementById("d2").innerHTML = new Date().toLocaleString();
  }, 1000);
 
+fetch('https://smhsdejiken.github.io/dejikenHP.github.io/BLOG/BL-js/blogs.json')
+  .then(response => response.json())
+  .then(data => {
+    // データを逆順に並べ替える
+    const reversedBlogs = data.reverse();
 
- 
+    const blogList = document.getElementById('recent-blog-list');
+
+    // リストをクリア（既存のリストがある場合に備えて）
+    blogList.innerHTML = '';
+
+    reversedBlogs.forEach(blog => {
+      const li = document.createElement('li');
+      const a = document.createElement('a');
+      a.href = blog.url;
+      a.textContent = blog.title;
+      li.appendChild(a);
+      blogList.appendChild(li);
+    });
+  });
