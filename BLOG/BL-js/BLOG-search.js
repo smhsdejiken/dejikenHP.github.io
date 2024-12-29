77414d51ce899242a0f739fd8d7d409b873a83d4
seven3000 <pingjingyaoping@gmail.com>
2024-12-30 01:03:57 +0900

    Update BLOG-search.js

--- BLOG/BL-js/BLOG-search.js
+++ BLOG/BL-js/BLOG-search.js
@@ -5,7 +5,7 @@ searchInput.addEventListener('input', () => {
   const keyword = searchInput.value.toLowerCase();
 const fs = require('fs');
 
-const blogsJson = fs.readFileSync('blogs.json', 'utf-8'); // ファイルから読み込み
+const blogsJson = fs.readFileSync('https://smhsdejiken.github.io/dejikenHP.github.io/BLOG/BL-js/blogs.json', 'utf-8'); // ファイルから読み込み
 
 const article = JSON.parse(blogsJson); // JSON 形式の文字列をオブジェクトに変換
 
