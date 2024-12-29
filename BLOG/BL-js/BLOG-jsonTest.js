e2f1d1f0c58139d2054022cdc1e5d34856165885
seven3000 <pingjingyaoping@gmail.com>
2024-12-29 23:30:10 +0900

    Update BLOG-jsonTest.js

--- BLOG/BL-js/BLOG-jsonTest.js
+++ BLOG/BL-js/BLOG-jsonTest.js
@@ -1,1 +1,18 @@
+fetch('blogs.json')
+  .then(response => response.json())
+  .then(data => {
+    const n = 3; // 表示する記事の数
+    const startIndex = Math.max(0, data.length - n);
+    const recentBlogs = data.slice(startIndex);
 
+    const blogList = document.getElementById('recent-blog-list'); // HTMLのul要素のid
+
+    recentBlogs.forEach(blog => {
+      const li = document.createElement('li');
+      const a = document.createElement('a');
+      a.href = blog.url;
+      a.textContent = blog.title;
+      li.appendChild(a);
+      blogList.appendChild(li);
+    });
+  });
